4aa91478541e0d4dcc92e09a3c24c1a0b0212adf
PP000777 <bia.silva007@hotmail.com>
2026-02-19 22:04:36 -0300

    Adicionei a funcionalidade de cadastrar produtos, ainda está em produção, nada completo, mas ainda consigo terminar em pouco tempo!!

--- project/cadastroLojinha.java
+++ project/cadastroLojinha.java
@@ -1,28 +1,63 @@
 package project;
-import java.util.Scanner;   
+
+import java.util.Scanner;
 
 public class cadastroLojinha {
     public static void cadastro(String[] args) {
         Scanner receber = new Scanner(System.in);
-        int contador_produtos = 0;
-        
-        System.out.println("Bem vindo à aba de cadastros!!");
-        System.out.println("Vamos verificar se nao esta cheia a lista");
+        menusLojinha.menu_cadastro(args);
+        int opcao = receber.nextInt();
 
-        while (true) {
-        if (contador_produtos == 5 ) {
-        break;
+        if (opcao == 1) {
+            menusLojinha.opcao_Slots(args);
+            int opcao2 = receber.nextInt();
+            if (dadosLojinha.nome_Produto[opcao2] != null) {
+                System.out.println("Qual o nome do produto? ");
+                dadosLojinha.nome_Produto[opcao2] = receber.nextLine();
+                System.out.println("Qual o valor desse produto? ");
+                dadosLojinha.valor_Produto[opcao2] = receber.nextInt();
+                System.out.println("PRODUTO CADASTRADO COM SUCESSO!!!");
+                System.out.println("Voltando para a página inicial...");
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException error) {
+                    System.out.println("Deu erro nessa bagaça");
+                }
+
+                mainLojinha.main(args);
+            }
+
+        } else {
+            System.out.println("Vou dar uma olhadinha se tem espaço pra colocar mais item aqui :D");
+            System.out.println("Verificando...");
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException error) {
+                System.out.println("Deu erro nessa bagaça");
+            }
         }
-        else{
-            System.out.println("Qual o nome do produto? ");
-            dadosLojinha.nome_Produto[contador_produtos] = receber.nextLine();
-            System.out.println("Qual o valor desse produto? ");
-            dadosLojinha.valor_Produto[contador_produtos] = receber.nextInt();
-            contador_produtos = contador_produtos + 1;
+        for (int i = 0; i < dadosLojinha.nome_Produto.length; i++) {
+            if (dadosLojinha.nome_Produto[i] != null) {
+                System.out.println("Qual o nome do produto? ");
+                dadosLojinha.nome_Produto[i] = receber.nextLine();
+                System.out.println("Qual o valor desse produto? ");
+                dadosLojinha.valor_Produto[i] = receber.nextInt();
+                System.out.println("verificaçãaaoooo");
+                System.out.println(dadosLojinha.valor_Produto[i]);
+                System.out.println(dadosLojinha.nome_Produto[i]);
+                break;
+            } else {
+                System.out.println("Todos os slots estão cheios parça");
+            }
+
+            
         }
-        }
+
+        System.out.println("cabo");
 
         receber.close();
-       
+
     }
 }
--- project/menusLojinha.java
+++ project/menusLojinha.java
@@ -1,5 +1,5 @@
 package project;
-
+import java.util.Scanner;
 public class menusLojinha {
     public static void menu(String[] args) {
         //MENU DA LOJINHA
@@ -24,5 +24,46 @@ public class menusLojinha {
         System.out.println("3- Camisa: R$190,00");
         System.out.println("4- Tênis da nike: R$450,00");
         System.out.println("5- Voltar ao menu para concluir sua compra");
+    }
+
+    public static void menu_cadastro(String[] args){
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("-------------------------------");
+        System.out.println("---------- CADASTROS ----------");
+        System.out.println("-------------------------------");
+        System.out.println("Vamos cadastrar alguns produtos!!");
+        System.out.println("Escolha como quer realizar seu cadastro: ");
+        System.out.println("1- Quero utilizar o slot que eu quiser");
+        System.out.println("2- Quero utilizar um slot vazio sequencial");
+        int opcao = leitor.nextInt();
+
+        if(opcao == 1){
+        int opcao2 = leitor.nextInt();
+        if (dadosLojinha.nome_Produto[opcao2] != null) {
+            
+        }
+        }else{
+        System.out.println("Vou dar uma olhadinha se tem espaço pra colocar mais item aqui :D");
+        System.out.println("Verificando...");
+
+        try{
+            Thread.sleep(1000);
+        }catch(InterruptedException error){
+            System.out.println("Deu erro nessa bagaça");
+        }
+        }
+       
+        leitor.close();
+        
+    }
+
+    public static void opcao_Slots(String[] args){
+        System.out.println("Qual slot quer usar?");
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
+        System.out.println("5");
     }
 }
