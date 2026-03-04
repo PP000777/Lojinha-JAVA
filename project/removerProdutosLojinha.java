4803b272a4c42ba7ffafa03cb76394095c76b2e2
PP000777 <bia.silva007@hotmail.com>
2026-03-04 20:10:50 -0300

    tirei o while sem motivo

--- project/removerProdutosLojinha.java
+++ project/removerProdutosLojinha.java
@@ -11,18 +11,14 @@ public class removerProdutosLojinha {
         if (opcaoo <= 0) {
             System.out.println("Preencha com uma opção válida, deve ser maior que 0!!");
         }else{
-            while (true) {
                 int opcaoRemover = opcaoo - 1;
                 if (dadosLojinha.nome_Produto[opcaoRemover] != null && dadosLojinha.valor_Produto[opcaoRemover] != 0){
                     dadosLojinha.nome_Produto[opcaoRemover] = null;
                     dadosLojinha.valor_Produto[opcaoRemover] = 0;
                     System.out.println("Produto removido com sucesso!!");
-                    break;
                 }else{
                     System.out.println("Esse produto não existe");
-                    break;
                 }        
-            }
         }
         
     }
