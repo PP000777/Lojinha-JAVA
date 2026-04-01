fbacd9a821f34e15d5444e7e64eb1355d68ae60c
PP000777 <bia.silva007@hotmail.com>
2026-04-01 16:59:50 -0300

    o mesmo problema do cadastro de objetos na função sequencial se aplicou para a selecionada, ele gerava uma a mais ao inves de atribuir valor. Logo, coloquei um set, para atribuir o valor como um novo produto

--- project/src/functions/cadastroLojinha.java
+++ project/src/functions/cadastroLojinha.java
@@ -20,11 +20,10 @@ public class cadastroLojinha {
             if (nomeProduto == null) {
                 System.out.println("Qual o nome do produto? ");
                 String nomeProdutoCadastro = entrada_dados_main.nextLine();
-                entrada_dados_main.nextLine();
                 System.out.println("Qual o valor desse produto? ");
                 double valorProdutoCadastro = entrada_dados_main.nextDouble();
                 entrada_dados_main.nextLine();
-                produtos.todos_produtoss.add(new produtos(nomeProdutoCadastro, valorProdutoCadastro));
+                produtos.todos_produtoss.set(opcaofinal ,new produtos(nomeProdutoCadastro, valorProdutoCadastro));
                 System.out.println("PRODUTO CADASTRADO COM SUCESSO!!!");
                 System.out.println("Voltando para a página inicial...");
                 try {
